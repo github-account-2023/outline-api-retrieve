097247db3955bdde2db569e6a2d52ae8f07f8412
Hailey2022 <96413516+Hailey2022@users.noreply.github.com>
2022-06-12 16:55:54 +0800

    Update retrieve.sh

--- retrieve.sh
+++ retrieve.sh
@@ -1,3 +1,4 @@
+[[ $EUID -ne 0 ]] && echo -e "This script must be run as root!" && exit 8964
 certSha256=$(cat /opt/outline/access.txt | head -n1 | sed 's#certSha256:##')
 apiUrl=$(cat /opt/outline/access.txt | sed -n "2,2p" | sed 's#apiUrl:##')
 echo "{'apiUrl':'"${apiUrl}"','certSha256':'"${certSha256}"'}"
